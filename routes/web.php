b7f3f376a86e3e9ae2886a16c6f05ec9b15788f5
Colin Sleiman <colinsleiman@gmail.com>
2026-04-07 23:48:08 +0300

    Added Home Page for Client

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,16 @@ use App\Http\Controllers\ServiceCategoryController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\Auth\Register;
 
-
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app', ['title' => 'Home']);
 });
 
+// auth routes
+Route::post('/register', [Register::class, '__invoke'])->middleware('guest');
+Route::post('/login', [Register::class, '__invoke'])->middleware('guest');
+Route::post('/logout', [Register::class, '__invoke'])->middleware('auth')->name('logout');
 
-Route::post('/register', [Register::class, '__invoke'])
-        ->middleware('guest');
-
-Route::post('/login', [Register::class, '__invoke'])
-        ->middleware('guest');
-
-Route::post('/logout', [Register::class, '__invoke'])
-        ->middleware('auth')
-        ->name('logout');
-
+// resources routes
 Route::resource('municipalities', MunicipalityController::class);
 Route::resource('offices', OfficeController::class);
 Route::resource('service-categories', ServiceCategoryController::class);
@@ -40,5 +33,3 @@ Route::Resource('documents', DocumentController::class);
 Route::Resource('document-requests', DocumentRequestController::class);
 Route::Resource('payments', PaymentController::class);
 
-// Note: Any custom method you create will not be automatically called by resource (only CRUD methods).
-// To use it, you must define a separate route.
